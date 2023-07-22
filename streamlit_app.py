104bfe345aa4c9c6bd6b7fbd611507dee0ab8074
Shrihari-369 <136453756+Shrihari-369@users.noreply.github.com>
2023-07-22 17:46:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from urllib.error import URLError
 
 streamlit.title('My Parents New Helthy Diner')
 
-streamlit.header('Breakfast Favorites')
+streamlit.header('Breakfast  Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
